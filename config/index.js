9e05eab52b72572906ee8ad01c61f56705dcc1b7
HuangChongqing <1756260160@qq.com>
2018-04-27 15:52:52 +0800

    开发环境配置

--- config/index.js
+++ config/index.js
@@ -3,12 +3,23 @@ var path = require('path')
 
 module.exports = {
   build: {
+    // env: require('./prod.env'),
+    // index: path.resolve(__dirname, '../public/index.html'),
+    // assetsRoot: path.resolve(__dirname, '../public'),
+    // assetsSubDirectory: 'static',
+    // assetsPublicPath: '/hcq-AM/public/',
+    // productionSourceMap: true,
+
+    // ChungKing 2018-4-26-23:17
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../public/index.html'),
-    assetsRoot: path.resolve(__dirname, '../public'),
+    port: 9000,
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/hcq-AM/public/',
+    assetsPublicPath: '',
     productionSourceMap: true,
+
+
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
